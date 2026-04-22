8a156f0e7d6f34637c5eb3c7e4e7c1aaccf8ac8c
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-04-22 12:01:46 +0900

    バリデーション用モジュールを記述

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    //whitelist: true,  //必要であれば。dtoに定義されていないプロパティを除外する(セキュリティ向上)
+  }));
   await app.listen(5000);
 }
 bootstrap();
