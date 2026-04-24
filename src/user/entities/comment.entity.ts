b12fbaacffa87df1d4955369cc2bbf0500d17c78
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-04-24 15:22:46 +0900

    commentのentity定義

---
+++ src/user/entities/comment.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from 'typeorm';
+import { User } from './user.entity';
+//import { Post } from './post.entity';
+
+// Commentsテーブルに対応するエンティティクラス
+@Entity('COMMENTS')
+export class Comment {
+  // 主キー（UUID文字列）
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+  //投稿ID
+  /*@ManyToOne((type) => Post, (post) => post.comments)
+  @JoinColumn({ name: 'post_id' })
+  post!: Post;*/
+
+  //コメントユーザーID
+  @ManyToOne(() => User, (user) => user.id)
+  @JoinColumn({ name: 'user_id' })
+  user!: User;
+
+  //コメント内容
+  @Column()
+  comment!: string;
+
+  //score　いいね数？
+  /*@Column()
+  score!: number;*/
+
+  //投稿日時
+  @Column({ type: 'datetime', name: 'created_at'})
+  created_at!: Date;
+}
