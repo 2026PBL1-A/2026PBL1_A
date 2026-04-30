39c38b0e3a4b6d5c69f58ebc99eefa31c397f851
N_A <kd1338248@st.kobedenshi.ac.jp>
2026-04-30 12:27:33 +0900

    不要なファイルの削除

--- src/routes/postRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createPostHandler } from '../controllers/postControllers';
-
-const router = express.Router();
-
-router.post('/', createPostHandler);
-
-export default router;
