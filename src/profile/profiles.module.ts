b205a4eba0102f80c271ca7a939d647b15c32ccf
N_A <kd1338248@st.kobedenshi.ac.jp>
2026-05-07 23:43:18 +0900

    プロフィールの更新API(ユーザー名,自己紹介文のみ)の追加

--- src/profile/profiles.module.ts
+++ src/profile/profiles.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfileController } from './profiles.controller';
 import { ProfileService } from './profiles.service';
 import { Profile } from './entities/profiles.entity';
+import { User } from '../user/entities/user.entity';
 
 // Profile機能のDI設定をまとめるモジュール
 @Module({
   // ProfileエンティティのRepositoryをServiceで利用可能にする
-  imports: [TypeOrmModule.forFeature([Profile])],
+  imports: [TypeOrmModule.forFeature([Profile, User])],
   controllers: [ProfileController],
   providers: [ProfileService],
   exports: [ProfileService],
