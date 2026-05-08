109e24258ef8f13f9ed76c5e66b5ab72fa37a4ca
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-08 15:22:37 +0900

    バリデーションにミスがあったので修正

--- src/comment/dto/create-comment.dto.ts
+++ src/comment/dto/create-comment.dto.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 //ユーザーデータの作成に必要なデータの型とバリデーションを定義
 export class CreateCommentDto {
     @IsString()
     @IsNotEmpty({ message: '内容を入力してください' })
     comment!: string;
-    postid!: string;
-    userid!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: '投稿IDを入力してください' })
+    postId!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: 'ユーザーIDを入力してください' })
+    userId!: string;
 }
