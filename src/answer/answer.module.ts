d88762596c1c44e3ef20de7be0f487bed39bfecc
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-08 14:11:15 +0900

    パス修正

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -3,13 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswerService } from './answer.service';
 import { AnswerController } from './answer.controller';
 import { Answer } from './entities/answer.entity';
-import { Question } from '../question/entities/question.entity';
+import { Questions } from '../questions/entities/questions.entity';
 import { User } from '../user/entities/user.entity';
 
 // Answer機能のDI設定をまとめるモジュール
 @Module({
   // AnswerエンティティのRepositoryをServiceで利用可能にする
-  imports: [TypeOrmModule.forFeature([Answer, Question, User])],
+  imports: [TypeOrmModule.forFeature([Answer, Questions, User])],
   controllers: [AnswerController],
   providers: [AnswerService],
   exports: [AnswerService],
