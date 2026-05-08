05757e3015094ed98fa55b0aaceb38cc5f54b618
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-08 14:56:52 +0900

    バリデーション書き直し

--- src/answer/dto/create-answer.dto.ts
+++ src/answer/dto/create-answer.dto.ts
@@ -1,9 +1,15 @@
-import { IsNotEmpty, IsString, IsInt } from 'class-validator';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 //ユーザーデータの作成に必要なデータの型とバリデーションを定義
 export class CreateAnswerDto {
     @IsString()
     @IsNotEmpty({ message: '内容を入力してください' })
     comment!: string;
-    questionid!: string;
-    userid!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: '質問IDを入力してください' })
+    questionId!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: 'ユーザーIDを入力してください' })
+    userId!: string;
 }
