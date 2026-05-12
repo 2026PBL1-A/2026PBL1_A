7e2d25fb9dcb55524199aaaafcf961b3c4fe209e
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-12 14:49:22 +0900

    moduleにentity追加

--- src/answer/answer.module.ts
+++ src/answer/answer.module.ts
@@ -5,11 +5,12 @@ import { AnswerController } from './answer.controller';
 import { Answer } from './entities/answer.entity';
 import { Questions } from '../questions/entities/questions.entity';
 import { User } from '../user/entities/user.entity';
+import { AnswerScore } from './entities/answerScore.entity';
 
 // Answer機能のDI設定をまとめるモジュール
 @Module({
   // AnswerエンティティのRepositoryをServiceで利用可能にする
-  imports: [TypeOrmModule.forFeature([Answer, Questions, User])],
+  imports: [TypeOrmModule.forFeature([Answer, Questions, User, AnswerScore])],
   controllers: [AnswerController],
   providers: [AnswerService],
   exports: [AnswerService],
