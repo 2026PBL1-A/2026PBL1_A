01807d063c6a8ecb5f42f6f6fad6ce396d53c0bb
N_A <kd1338248@st.kobedenshi.ac.jp>
2026-05-12 15:00:22 +0900

    moduleファイルの変更

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Questions } from './entities/questions.entity';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
+import { QuestionTag } from '../question-tags/entities/question-tags.entity';
 
 @Module({
     imports: [
-        TypeOrmModule.forFeature([Questions]),
+        TypeOrmModule.forFeature([Questions, QuestionTag]),
     ],
     controllers: [QuestionsController],
     providers: [QuestionsService],
