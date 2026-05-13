52a281de39ad467f5b8d8d24accffe75d8ae3702
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-13 11:48:12 +0900

    ログインdtoにstメールバリデーション追加

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,6 +1,7 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, Matches } from 'class-validator';
 
 export class LoginDto {
+  @Matches(/^[a-zA-Z0-9]*@st\.kobedenshi\.ac\.jp$/, { message: 'stメールを入力してください' })
   @IsEmail()
   @IsNotEmpty({ message: '正しいメールアドレス形式で入力してください' })
   email!: string;
