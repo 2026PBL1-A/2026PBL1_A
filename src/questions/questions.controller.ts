b3c029c3991d7757cba023f3f99676ba4eadaafe
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-15 09:40:47 +0900

    キーワード検索APIのルート

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get, Body, Param, Req, UseGuards, } from '@nestjs/common';
+import { Controller, Post, Get, Body, Param, Query, Req, UseGuards, } from '@nestjs/common';
 import { Request } from 'express';
 import { QuestionsService } from './questions.service';
 import { CreateQuestionDto } from './dto/create-question.dto';
@@ -6,6 +6,7 @@ import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 interface AuthenticatedRequest extends Request {
     user: { userId: string };
+    keyword: {keyword: string};
 }
 
 @Controller('questions')
@@ -32,8 +33,15 @@ export class QuestionsController {
         return this.questionsService.findAll();
     }
 
+    @Get('search')//キーワードで質問を検索
+    searchQuestionsByKeyword(@Query('keyword') keyword: string) {
+        return this.questionsService.searchQuestionsByKeyword(keyword);
+    }
+    
     @Get(':id')//指定した質問を取得
     findOne(@Param('id') id: string) {
         return this.questionsService.findOne(id);
     }
+
+
 }
