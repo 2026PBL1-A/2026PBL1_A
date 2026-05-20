1a0ea8fcaa08f641c19f702b6d83714205c93267
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-20 11:33:59 +0900

    回答更新のdto

---
+++ src/answers/dto/update-answer.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { Transform } from 'class-transformer';
+//回答の更新に必要なデータの型とバリデーションを定義
+export class UpdateAnswerDto {
+    @IsString()
+    @IsNotEmpty({ message: '内容を入力してください' })
+    @Transform(({ value }) => value.trim()) // 空白入力のみを防止
+    answer!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: '質問IDを入力してください' })
+    postId!: string;
+
+    @IsUUID()
+    @IsNotEmpty({ message: 'ユーザーIDを入力してください' })
+    userId!: string;
+}
