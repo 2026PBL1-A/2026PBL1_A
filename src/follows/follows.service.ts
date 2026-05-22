67d0169366d10ef3db3c8b9b172062bf826d80b6
Yume Ishii <kd1370870@st.kobedenshi.ac.jp>
2026-05-22 12:23:52 +0900

    フォロー追加・解除API作成

--- src/follows/follows.service.ts
+++ src/follows/follows.service.ts
@@ -1,7 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository, TypeOrmModule } from '@nestjs/typeorm';
+import { Repository, DeepPartial } from 'typeorm';
+import { Users } from '../users/entities/users.entity';
+import { Follows } from './entities/follows.entity';
 
 // followsテーブルに対するデータ操作を担当するサービス
 @Injectable()
 export class FollowsService {
-    
+    constructor(
+            @InjectRepository(Users)
+            private readonly userRepository: Repository<Users>,
+            @InjectRepository(Follows)
+            private readonly followRepository: Repository<Follows>,
+        ) {}
+
+    async create(followingId: string, followerId: string) {
+        if (followingId === followerId) {
+            throw new Error('ユーザーは自分自身をフォローできません');
+        }
+        if (!followingId || !followerId) {
+            throw new Error('フォローするユーザーIDとフォロワーのユーザーIDが指定されていません');
+        }
+
+        const follower = await this.followRepository.findOneBy({ following_id: followingId, follower_id: followerId });
+        
+        // 既にフォローしている場合は解除する
+        if (follower) {
+            return await this.followRepository.delete({ following_id: followingId, follower_id: followerId });
+        }
+        else {
+            return await this.followRepository.save({ following_id: followingId, follower_id: followerId });
+        }
+    }
 }
