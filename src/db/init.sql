352e3229c37e8aff1c53b9c27e20cf049c0cbb62
y_k <ka.20.zu.04@gmail.com>
2026-05-22 10:39:00 +0900

    フォローテーブルの作成

--- src/db/init.sql
+++ src/db/init.sql
@@ -141,3 +141,12 @@ CREATE TABLE IF NOT EXISTS QUESTION_IMAGES (
   UNIQUE (question_id, sort_order),
   FOREIGN KEY (question_id) REFERENCES QUESTIONS(id) ON DELETE CASCADE
 );
+
+-- followsテーブル作成(テーブル大文字)
+CREATE TABLE IF NOT EXISTS FOLLOWS (
+  follower_id VARCHAR(36) NOT NULL,
+  following_id VARCHAR(36) NOT NULL,
+  PRIMARY KEY (follower_id, following_id),
+  FOREIGN KEY (follower_id) REFERENCES USERS(id) ON DELETE CASCADE,
+  FOREIGN KEY (following_id) REFERENCES USERS(id) ON DELETE CASCADE
+);
